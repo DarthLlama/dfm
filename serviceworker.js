81fe8fa93cbc685c17494bea062691e46d9fd74d
MCDONALD Alexander C <m020457e@student.staffs.ac.uk>
2017-03-14 16:06:59 +0000

    Service Worker - Offline stuff, CacheStorageApi #3

--- serviceworker.js
+++ serviceworker.js
@@ -12,3 +12,12 @@ self.addEventListener('install', function(event) {
     })
   );
 });
+
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    fetch(event.request).catch(function() {
+      return caches.match('offline.html');
+    })
+  );
+});
