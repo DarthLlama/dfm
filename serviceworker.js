2dd15f57f807748ae380cb23e268ae76c6f5f663
MCDONALD Alexander C <m020457e@student.staffs.ac.uk>
2017-03-14 16:06:38 +0000

    Service Worker - Offline stuff, CacheStorageApi #3

--- serviceworker.js
+++ serviceworker.js
@@ -1,15 +1,14 @@
+var CACHE_NAME = 'gih-cache';
+var CACHED_URLS = [
+  'offline.html',
+  'mystyles.css’,
+  'dino.png'
+];
+
 self.addEventListener('install', function(event) {
   event.waitUntil(
-    caches.open('gih-cache').then(function(cache) {
-      return cache.add('offline.html');
-    })
-  );
-});
-
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    fetch(event.request).catch(function() {
-      return caches.match('offline.html');
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(CACHED_URLS);
     })
   );
 });
