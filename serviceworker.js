0468fe345b1a2fec0a07c89bd80bbfe82cb75781
MCDONALD Alexander C <m020457e@student.staffs.ac.uk>
2017-03-28 16:38:43 +0100

    Adding lots of images

--- serviceworker.js
+++ serviceworker.js
@@ -1,35 +1,36 @@
 var CACHE_NAME = 'gih-cache-v5';
+var BASE_PATH = '/dfm/';
 var CACHED_URLS = [
   // Our HTML
-  'first.html',
+  BASE_PATH + 'first.html',
   // Stylesheets and fonts
-    'min-style.css',
-    'styles.css',
-    'mystyles.css',
+    BASE_PATH + 'min-style.css',
+    BASE_PATH + 'styles.css',
+    BASE_PATH + 'mystyles.css',
     'https://fonts.googleapis.com/css?family=Roboto:regular,bold,italic,thin,light,bolditalic,black,medium&lang=en',
     'https://fonts.googleapis.com/icon?family=Material+Icons',
   // JavaScript
-    'material.js',
+    BASE_PATH + 'material.js',
   // Images
-    'appimages/android-icon-36x36.png',
-    'appimages/android-icon-48x48.png',
-    'appimages/android-icon-72x72.png',
-    'appimages/android-icon-96x96.png',
-    'appimages/android-icon-144x144.png',
-    'appimages/android-icon-192x192.png',
-    'appimages/paddy.jpg',
-    'appimages/favicon.ico',
-    'appimages/favicon-16x16.ico',
-    'appimages/favicon-96x96.ico',
-    'appimages/favicon.ico',
-    'eventsimages/example-blog01.jpg',
-    'eventsimages/example-blog02.jpg',
-    'eventsimages/example-blog03.jpg',
-    'eventsimages/example-blog04.jpg',
-    'eventsimages/example-blog05.jpg',
-    'eventsimages/example-blog06.jpg',
-    'eventsimages/example-blog07.jpg',
-    'eventsimages/example-blog08.jpg'
+    BASE_PATH + 'appimages/android-icon-36x36.png',
+    BASE_PATH + 'appimages/android-icon-48x48.png',
+    BASE_PATH + 'appimages/android-icon-72x72.png',
+    BASE_PATH + 'appimages/android-icon-96x96.png',
+    BASE_PATH + 'appimages/android-icon-144x144.png',
+    BASE_PATH + 'appimages/android-icon-192x192.png',
+    BASE_PATH + 'appimages/paddy.jpg',
+    BASE_PATH + 'appimages/favicon.ico',
+    BASE_PATH + 'appimages/favicon-16x16.ico',
+    BASE_PATH + 'appimages/favicon-96x96.ico',
+    BASE_PATH + 'appimages/favicon.ico',
+    BASE_PATH + 'eventsimages/example-blog01.jpg',
+    BASE_PATH + 'eventsimages/example-blog02.jpg',
+    BASE_PATH + 'eventsimages/example-blog03.jpg',
+    BASE_PATH + 'eventsimages/example-blog04.jpg',
+    BASE_PATH + 'eventsimages/example-blog05.jpg',
+    BASE_PATH + 'eventsimages/example-blog06.jpg',
+    BASE_PATH + 'eventsimages/example-blog07.jpg',
+    BASE_PATH + 'eventsimages/example-blog08.jpg'
     
 ];
 
@@ -44,7 +45,7 @@ self.addEventListener('install', function(event) {
 
 self.addEventListener('fetch', function(event) {
   var requestURL = new URL(event.request.url);
-  if (requestURL.pathname === 'first.html') {
+  if (requestURL.pathname === BASE_PATH + 'first.html') {
     event.respondWith(
       caches.open(CACHE_NAME).then(function(cache) {
         return cache.match('first.html').then(function(cachedResponse) {
