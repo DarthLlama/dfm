da3f06811bbc1260f4dfb23446cf557096b31c69
MCDONALD Alexander C <m020457e@student.staffs.ac.uk>
2017-04-04 15:15:23 +0100

    Service Workers Week 4

--- serviceworker.js
+++ serviceworker.js
@@ -1,29 +1,31 @@
-var CACHE_NAME = 'gih-cache-v5';
 var BASE_PATH = '/dfm/';
+var CACHE_NAME = 'gih-cache-v6';
 var CACHED_URLS = [
-  // Our HTML
-  BASE_PATH + 'first.html',
-  // Stylesheets and fonts
-    BASE_PATH + 'min-style.css',
-    BASE_PATH + 'styles.css',
-    BASE_PATH + 'mystyles.css',
-    BASE_PATH + 'offline-map.js',
-    'https://fonts.googleapis.com/css?family=Roboto:regular,bold,italic,thin,light,bolditalic,black,medium&lang=en',
-    'https://fonts.googleapis.com/icon?family=Material+Icons',
-  // JavaScript
-    BASE_PATH + 'material.js',
-  // Images
+    // Our HTML
+    BASE_PATH + 'first.html',
+    
+    // Images for favicons
     BASE_PATH + 'appimages/android-icon-36x36.png',
     BASE_PATH + 'appimages/android-icon-48x48.png',
     BASE_PATH + 'appimages/android-icon-72x72.png',
     BASE_PATH + 'appimages/android-icon-96x96.png',
     BASE_PATH + 'appimages/android-icon-144x144.png',
     BASE_PATH + 'appimages/android-icon-192x192.png',
+    BASE_PATH + 'appimages/favicon-32x32.png',
+
+    //Images for page
+    BASE_PATH + 'appimages/offlinemap.jpg',
+    BASE_PATH + 'appimages/dino.png',
+    BASE_PATH + 'appimages/jack.jpg',
     BASE_PATH + 'appimages/paddy.jpg',
     BASE_PATH + 'appimages/favicon.ico',
-    BASE_PATH + 'appimages/favicon-16x16.ico',
-    BASE_PATH + 'appimages/favicon-96x96.ico',
-    BASE_PATH + 'appimages/favicon.ico',
+    BASE_PATH + 'appimages/favicon-16x16.png',
+    BASE_PATH + 'appimages/favicon-32x32.png',
+    BASE_PATH + 'appimages/favicon-96x96.png',
+    BASE_PATH + 'appimages/ms-icon-70x70.png',
+    BASE_PATH + 'appimages/ms-icon-144x144.png',
+    BASE_PATH + 'appimages/ms-icon-150x150.png',
+    BASE_PATH + 'appimages/ms-icon-310x310.png',
     BASE_PATH + 'eventsimages/example-blog01.jpg',
     BASE_PATH + 'eventsimages/example-blog02.jpg',
     BASE_PATH + 'eventsimages/example-blog03.jpg',
@@ -31,14 +33,31 @@ var CACHED_URLS = [
     BASE_PATH + 'eventsimages/example-blog05.jpg',
     BASE_PATH + 'eventsimages/example-blog06.jpg',
     BASE_PATH + 'eventsimages/example-blog07.jpg',
-    BASE_PATH + 'eventsimages/example-blog08.jpg',
-    BASE_PATH + 'https://maps.googleapis.com/maps/api/js?key=AIzaSyDDCkYNBfhXUt6Ogx_ITtmNdjt0w9oW1rE&callback=initMap',
-    BASE_PATH + 'https://maps.googleapis.com/maps/api/staticmap?center=53.0027,2.1794&zoom=2&size=600x100&key=AIzaSyDDCkYNBfhXUt6Ogx_ITtmNdjt0w9oW1rE);'
-    
+    BASE_PATH + 'eventsimages/example-work01.jpg',
+    BASE_PATH + 'eventsimages/example-work02.jpg',
+    BASE_PATH + 'eventsimages/example-work03.jpg',
+    BASE_PATH + 'eventsimages/example-work04.jpg',
+    BASE_PATH + 'eventsimages/example-work05.jpg',
+    BASE_PATH + 'eventsimages/example-work06.jpg',
+    BASE_PATH + 'eventsimages/example-work07.jpg',
+    BASE_PATH + 'eventsimages/example-work08.jpg',
+    BASE_PATH + 'eventsimages/example-work09.jpg',  
+    // JavaScript
+    BASE_PATH + 'offline-map.js',
+    BASE_PATH + 'material.js',
+    // Manifest
+    BASE_PATH + 'manifest.json',
+  // CSS and fonts
+    'https://fonts.googleapis.com/css?family=Roboto:regular,bold,italic,thin,light,bolditalic,black,medium&lang=en',
+    'https://fonts.googleapis.com/icon?family=Material+Icons',
+    BASE_PATH + 'min-style.css',
+    BASE_PATH + 'styles.css'
 ];
 
+var googleMapsAPIJS = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyDDCkYNBfhXUt6Ogx_ITtmNdjt0w9oW1rE&callback=initMap';
+
 self.addEventListener('install', function(event) {
-  // Cache everything in CACHED_URLS. Installation will fail if something fails to cache
+  // Cache everything in CACHED_URLS. Installation fails if anything fails to cache
   event.waitUntil(
     caches.open(CACHE_NAME).then(function(cache) {
       return cache.addAll(CACHED_URLS);
@@ -48,6 +67,7 @@ self.addEventListener('install', function(event) {
 
 self.addEventListener('fetch', function(event) {
   var requestURL = new URL(event.request.url);
+  // Handle requests for index.html
   if (requestURL.pathname === BASE_PATH + 'first.html') {
     event.respondWith(
       caches.open(CACHE_NAME).then(function(cache) {
@@ -60,7 +80,7 @@ self.addEventListener('fetch', function(event) {
         });
       })
     );
-       // Handle requests for Google Maps JavaScript API file
+ // Handle requests for Google Maps JavaScript API file
   } else if (requestURL.href === googleMapsAPIJS) {
     event.respondWith(
       fetch(
@@ -70,15 +90,15 @@ self.addEventListener('fetch', function(event) {
         return caches.match('offline-map.js');
       })
     );
-      
   } else if (
     CACHED_URLS.includes(requestURL.href) ||
-    CACHED_URLS.includes(requestURL.pathname) {
+    CACHED_URLS.includes(requestURL.pathname)
+  ) {
     event.respondWith(
       caches.open(CACHE_NAME).then(function(cache) {
         return cache.match(event.request).then(function(response) {
           return response || fetch(event.request);
-        })
+        });
       })
     );
   }
@@ -98,3 +118,8 @@ self.addEventListener('activate', function(event) {
     })
   );
 });
+
+
+
+
+
